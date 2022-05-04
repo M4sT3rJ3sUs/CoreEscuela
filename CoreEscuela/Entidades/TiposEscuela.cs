de256db9c777f2a09075c83ec41d457b782491b0
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2022-05-04 16:25:35 -0500

    Agregar archivos de proyecto.

---
+++ CoreEscuela/Entidades/TiposEscuela.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreEscuela.Entidades
+{
+    public enum TiposEscuela
+    {
+        Primaria,
+        Secundaria,
+        PreEscolar
+    }
+}
---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace CoreEscuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
+                            ciudad: "Bogotá", pais: "Colombia"
+                            );
+                Console.WriteLine(escuela);
+            }
+
+        }
+    }
+}
