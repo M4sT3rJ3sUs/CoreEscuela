95a10aeee5f8a0e037840b708a3add8645d0d434
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2022-05-04 18:02:54 -0500

    Cambios video 16.

---
+++ CoreEscuela/Entidades/TiposJornada.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreEscuela.Entidades
+{
+    public enum TiposJornada
+    {
+        Mañana, Tarde, Noche
+    }
+}
--- CoreEscuela/Program.cs
+++ CoreEscuela/Program.cs
@@ -7,14 +7,39 @@ namespace CoreEscuela
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
-                            ciudad: "Bogotá", pais: "Colombia"
-                            );
-                Console.WriteLine(escuela);
-            }
+            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
+            ciudad: "Bogotá", pais: "Colombia"
+            );
 
+            var arregloCursos = new Curso[3];
+            arregloCursos[0] = new Curso()
+            {
+                Nombre = "101"
+            };
+            var curso2 = new Curso()
+            {
+                Nombre = "201"
+            };
+            arregloCursos[1] = curso2;
+            arregloCursos[2] = new Curso
+            {
+                Nombre = "301"
+            };
+            Console.WriteLine(escuela);
+            System.Console.WriteLine("-----");
+            Imprimircursos(arregloCursos);
+        }
+
+        private static void Imprimircursos(Curso[] arregloCursos)
+        {
+            int contador=0;
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre }, Id {arregloCursos[contador].UniqueId}");
+
+
+                contador = contador + 1;
+            }
         }
     }
 }
